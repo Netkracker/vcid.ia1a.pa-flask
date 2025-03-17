27e310c10a90534df879e381031c27ee3a13e345
Netkracker <ampoule_legends.0r@icloud.com>
2025-03-17 09:33:38 +0100

    heroku changes

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supergeheimespasswort")
     # Datenbankverbindung: Nutzt eine Umgebungsvariable oder eine lokale PostgreSQL-Datenbank als Fallback
     #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgres:Welcome$2025@localhost/nomnom")
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     # Deaktiviert das Tracking von Modifikationen durch SQLAlchemy, um Ressourcen zu sparen
     SQLALCHEMY_TRACK_MODIFICATIONS = False
